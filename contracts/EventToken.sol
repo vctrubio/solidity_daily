e8f01459a4739db71a0ade0dce441b6870819899
vctrubio <vctrubio@gmail.com>
2025-03-29 20:51:56 +0100

    orcastrate the problem

---
+++ contracts/EventToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/*
+
+Problem:
+- i want to track a which employee gives out which ticket and then see who attends the event. reward should be given to the employee wallet
+- we have a contract for THE event 
+  - it has different tokens assigend to different employee. 
+  - One token = One employee
+- Employee trade/or mint token to users
+- Users, if attent event, employee is rewarded
+- event has time, duration, attendence array/or mapping
+
+Date: 29.05.25
+
+*/
